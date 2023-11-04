bbe739d9a11d8077621fd584878dbe237fc8a5aa
Ibrahim Koray Aksoy <ib.koray@gmail.com>
2023-11-04 12:44:38 +0100

    make dist visible after running 'nx run-many --target=build --all --configuration=development --verbose'

---
+++ dist/shared/src/lib/shared.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.shared = void 0;
+function shared() {
+    return 'shared';
+}
+exports.shared = shared;
+//# sourceMappingURL=shared.js.map
